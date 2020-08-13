f49cc95e8e05800f629d6c5196fae9fda02a5000
Megan Lee <megselayne@gmail.com>
2020-08-13 16:23:16 -0400

    updating seed file

--- db/202008111520_add_rows_climbing_routes.sql
+++ db/202008111520_add_rows_climbing_routes.sql
@@ -1,5 +1,6 @@
 INSERT INTO climbing_routes
 (name, type, rating, image, longitude, latitude)
 VALUES
-('Too Big To Flail', 'Boulder', 'v10', 'https://www.mountainproject.com/photo/114260820/the-beauty', -118.5768, 37.3356)
+('Too Big To Flail', 'Boulder', 'v10', 'https://cdn2.apstatic.com/photos/climb/114260820_medium_1523759870.jpg', -118.5768, 37.3356),
+('The Nose', 'Trad', '5.9 C2', 'https://cdn2.apstatic.com/photos/climb/111184613_medium_1494342297.jpg',-119.6383, 37.7305)
 ;
