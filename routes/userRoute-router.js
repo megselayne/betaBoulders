4aad76851a5ba3da29ba8a0d82d8d1076af7d7a8
Megan Lee <megselayne@gmail.com>
2020-08-13 20:01:45 -0400

    setting up show view

--- routes/userRoute-router.js
+++ routes/userRoute-router.js
@@ -4,6 +4,11 @@ const userRouteRouter = express.Router();
 const userRoutesController = require('../controllers/user-routes-controller');
 const authHelpers = require('../services/auth/auth-helpers');
 
+userRouteRouter.get('/:id([0-9]+)', userRoutesController.show, (req, res) => {
+    res.render('users/show', {
+        userRoute: res.locals.userRoute
+    })
+})
 userRouteRouter.post('/:id([0-9]+)',authHelpers.loginRequired, userRoutesController.create);
 userRouteRouter.get('/edit/:id([0-9]+)', userRoutesController.show, (req, res) => {
     res.render('users/edit', {
