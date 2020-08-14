242adcd2e5b669b6afafa60027b58b501cd009c3
Megan Lee <megselayne@gmail.com>
2020-08-14 14:43:13 -0400

    cleaned up nav highlight to account for redirects

--- public/src/main.js
+++ public/src/main.js
@@ -2,10 +2,7 @@
 console.log('main.js is connected!');
 
 //variable declarations
-const home = document.querySelector('#home');
-const routes = document.querySelector('#routes');
-const profile = document.querySelector('#profile');
-const logout = document.querySelector('#log');
+const navLi = document.querySelectorAll('li a');
 let activate = null;
 //functions
 
@@ -13,28 +10,26 @@ let activate = null;
 
 //local storage
 const localStorageTab =()=> {
-    if(localStorage.getItem('activeTab')){
-       const now = localStorage.getItem('activeTab');
-       document.getElementById(`${now}`).classList.add('active');
+    if(window.location.pathname === '/'){
+        localStorage.setItem('activeTab','home');
     }
-    else{
-        home.classList.add('active');
+    else if(window.location.pathname.includes('/routes')){
+        localStorage.setItem('activeTab', 'routes');
     }
-}
-//removes active class from all, adds it back to clicked target
-const activeTab = (event)=>{
-    console.log('heard click');
-    console.log(event.target.id);
-    let id = event.target.id;
-    localStorage.setItem('activeTab', id);
-}
+    else if(window.location.pathname.includes('/user')){
+        localStorage.setItem('activeTab', 'profile');
+    }
+    else if(window.location.pathname.includes('/auth')){
+        localStorage.setItem('activeTab', 'log');
+    }
+    navLi.forEach((element) => {
+        if(element.id === localStorage.getItem('activeTab')){
+            element.classList.add('active');
+        }
+    })
 
-
+}
 
 
 // //event listeners
-home.addEventListener('click', activeTab);
-routes.addEventListener('click', activeTab);
-profile.addEventListener('click', activeTab);
-logout.addEventListener('click', activeTab);
 window.addEventListener('load', localStorageTab);
